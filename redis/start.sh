22d7d36ec15edbd6818f29ef07e7b6508f1bf4fb
Tom X Nguyen <tom81094@gmail.com>
2026-07-25 04:03:08 +0700

    fix(redis): start after preparing Railway volume

--- redis/start.sh
+++ redis/start.sh
@@ -2,4 +2,4 @@
 set -eu
 mkdir -p /var/lib/redis/data
 chown -R redis:redis /var/lib/redis
-exec gosu redis redis-server --dir /var/lib/redis/data --appendonly yes --maxmemory-policy allkeys-lru
+exec redis-server --dir /var/lib/redis/data --appendonly yes --maxmemory-policy allkeys-lru
